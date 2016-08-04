111d7a23ab598907b78656d7339957a2c95b6180
Facundo La Rocca <facundo_larocca@yahoo.com.ar>
2016-08-04 10:23:01 -0300

    Ajuste del ejemplo de restify

--- example.js
+++ example.js
@@ -1,78 +1,22 @@
 var restify = require('restify');
 var paramReceived = null;
-var MongoRepository = require('./repositories/MongoRepository');
+var MongoRepository = require('./src/repositories/MongoRepository');
  
 function testMondoDB() {
-  // console.log('2');
-  // var mongodb = require('mongodb');
-  // console.log('3');
- 
-  // //We need to work with "MongoClient" interface in order to connect to a mongodb server.
-  // var MongoClient = mongodb.MongoClient;
-  // console.log('4');
-
-  // Connection URL. This is where your mongodb server is running.
   var url = 'mongodb://localhost:27017/yojuego';
   var repo = new MongoRepository(url);
-  repo.connect();
-  if (repo.isConnected()){
-    var user = {name: 'facundo', surname:'la rocca'};
-    repo.insert(user);
+  repo.connect()
+  .then(() => {
+                console.log('resolve connect');
+                var user = {name: 'facundo', surname:'la rocca'};
+                repo.insert('user', user)
+                .then(() => console.log('resolve insert'), (e) => console.log('reject catch: ' + e))
+                .catch((e) => console.log('reject insert err: ' + e));
 
-    console.log("insert realizado");
-  }else{
-    console.log("No su pudo conectar con el server");
-  }
-  console.log('5');
+              }, 
+        (e) => console.log('reject err: ' + e))
+  .catch((e) => console.log('catch err: ' + e));
 
-
-
-  // Use connect method to connect to the Server
-  // MongoClient.connect(url, function (err, db) {
-  //   console.log('6 - err: ' + err);
-  //   if (err) {
-  //     console.log('7 - Unable to connect to the mongoDB server. Error:', err);
-  //   } else {
-  //     //HURRAY!! We are connected. :)
-  //     console.log('8 - Connection established to', url);
-
-  //     var adminRol = {name: 'admin', access: 'full'};
-  //     var moderatorRol = {name: 'moderator', access: 'limited'};
-  //     var userRol = {name: 'user', access: 'none'};
-  //     var superAdminRol = {name: 'super-admin', access: 'full+grants'};
-
-  //     var user1 = {name: 'modulus admin', age: 42, roles: [adminRol, moderatorRol, userRol]};
-  //     var user2 = {name: 'modulus user', age: 22, roles: [userRol]};
-  //     var user3 = {name: 'modulus super admin', age: 92, roles: [superAdminRol, adminRol, moderatorRol, userRol]};
-
-  //     var users = db.collection('users');
-
-  //     users.insert([user1, user2, user3], function (err, result) {
-  //       if (err) {
-  //         console.log('Error inserting users. ', err);
-  //       } else {
-  //         console.log('Inserted %d documents into the "users" collection. The documents inserted with "_id" are:', result.length, result);
-  //       }
-  //     });
-
-  //     var roles = db.collection('users');
-  //     roles.find({name: 'modulus user'}).toArray(function (err, result) {
-  //       if (err) {
-  //         console.log(err);
-  //       } else if (result.length) {
-  //         console.log('Found:', result);
-  //       } else {
-  //         console.log('No document(s) found with defined "find" criteria!');
-  //       }
-  //     });
-
-  //     //Close connection
-  //     db.close();
-  //   }
-  //   console.log('9');
-  // });
-
-  console.log('10');
 }
 
 function getMessage(req, res, next) {
