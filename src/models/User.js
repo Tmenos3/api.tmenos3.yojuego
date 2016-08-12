d636e2e588f729202cae4c52ba0210e3f81cc12e
Facundo La Rocca <facundo_larocca@yahoo.com.ar>
2016-08-12 17:46:27 -0300

    Implementacio User

--- src/models/User.js
+++ src/models/User.js
@@ -1,10 +1,45 @@
+'use strict'
+
+ var CommonValidatorHelper = require('../helpers/CommonValidator/CommonValidatorHelper');
+ var NotNullOrUndefinedCondition = require('../helpers/CommonValidator/NotNullOrUndefinedCondition');
+ var CustomCondition = require('../helpers/CommonValidator/CustomCondition');
 // get an instance of mongoose and mongoose.Schema
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+// var mongoose = require('mongoose');
+// var Schema = mongoose.Schema;
+
+class User{
+    constructor(username, password){
+        var conditions = [
+            new NotNullOrUndefinedCondition(username, User.INVALID_USERNAME()),
+            new NotNullOrUndefinedCondition(password, User.INVALID_PASSWORD()),
+            new CustomCondition(() => { return username !== password }, User.PASSWORD_CANNOT_BE_EQUAL_TO_USERNAME())
+        ];
+        
+        var validator = new CommonValidatorHelper(conditions, () => { 
+            this.username = username;
+            this.password = password;  
+        }, (err) => { throw new Error(err); });
+        validator.execute();
+    }
+
+    static INVALID_USERNAME(){
+        return 'Debe proveer un username válido (al menos 5 caracteres entre letras, numeros, puntos y unserscore).';
+    }
+
+    static INVALID_PASSWORD(){
+        return 'Debe proveer un username válido (al menos 5 caracteres).';
+    }
+
+    static PASSWORD_CANNOT_BE_EQUAL_TO_USERNAME(){
+        return 'El password no puede ser igual al username.';
+    }
+}
+
+module.exports = User;
 
 // set up a mongoose model and pass it using module.exports
-module.exports = mongoose.model('User', new Schema({ 
-    username: String, 
-    password: String, 
-    admin: Boolean 
-}));
+// module.exports = mongoose.model('User', new Schema({ 
+//     username: String, 
+//     password: String, 
+//     admin: Boolean 
+// }));
