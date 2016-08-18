8aea5c66518cc86576c24e920e3e3ca0d9905372
Facundo Cacho <facucacho10@gmail.com>
2016-08-18 20:06:43 -0300

    match test partial commit

---
+++ __tests__/src/routes/MatchRoutes-test.js
@@ -0,0 +1,12 @@
+describe('MatchRoutes', () => {
+  var server;
+
+  beforeEach(function() {
+    // server = require('mongodb');
+    // mongodb.MongoClient.connect = mockedConnect(false, {});
+  });
+
+  afterEach(function() {
+    // mongodb = null;
+  });
+});
