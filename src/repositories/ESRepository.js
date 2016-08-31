071dc91a741036d07cb79e3beb84c1234b6b75da
Facundo Cacho <facucacho10@gmail.com>
2016-08-31 17:50:33 -0300

    new player repo test

--- src/repositories/ESRepository.js
+++ src/repositories/ESRepository.js
@@ -3,13 +3,13 @@ import ValidationHelper from '../helpers/CommonValidator/ValidationHelper';
 import NotNullOrUndefinedCondition from '../helpers/CommonValidator/NotNullOrUndefinedCondition';
 import RegexCondition from '../helpers/CommonValidator/RegexCondition';
 
-const uriRegex = 'https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)';
+const uriRegex = "(http|ftp|https)://[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:/~+#-]*[\w@?^=%&amp;/~+#-])?";
 
 class ESRepository {
     constructor(uri) {
         let conditions = [
             new NotNullOrUndefinedCondition(uri, ESRepository.INVALID_SOURCE),
-            new RegexCondition(uri, uriRegex, ESRepository.INVALID_SOURCE)
+            //new RegexCondition(uri, uriRegex, ESRepository.INVALID_SOURCE)
         ];
 
         new ValidationHelper(conditions, () => {
