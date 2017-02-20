fd00f5e35db55a7bbc62896719a5b5869e35a0a2
Facundo La Rocca <frocca@web.web.com>
2017-02-20 17:39:44 -0300

    Friendship repo refactor

--- src/repositories/FriendshipESRepository.js
+++ src/repositories/FriendshipESRepository.js
@@ -4,123 +4,67 @@ let Friendship = require('../models/Friendship');
 class FriendshipESRepository extends ESRepository {
     constructor(client) {
         super(client);
+
+        this._mapFriendship = this._mapFriendship.bind(this);
+        this._getQueryByPlayerId = this._getQueryByPlayerId.bind(this);
+        this._getQueryByFriendId = this._getQueryByFriendId.bind(this);
+        this._getDocument = this._getDocument.bind(this);
     }
 
     get(friendshipId) {
-        return new Promise((resolve, reject) => {
-            super.get(friendshipId, 'yojuego', 'friendship')
-                .then((objRet) => {
-                    if (objRet.code == 404) {
-                        resolve({ code: 404, message: 'Friendship does not exist', resp: null });
-                    } else {
-                        let friendship = new Friendship(objRet.resp._source.playerId, objRet.resp._source.friendId, objRet.resp._source.status, objRet.resp._source.email);
-                        friendship._id = objRet.resp._id;
-                        friendship.friendshipAudit = objRet.resp._source.friendshipAudit;
-                        resolve({ code: 200, message: null, resp: friendship });
-                    }
-                }, reject);
-        });
+        return super.get(friendshipId, 'yojuego', 'friendship')
+            .then((objRet) => {
+                if (objRet.code == 404) {
+                    resolve({ code: 404, message: 'Friendship does not exist', resp: null });
+                } else {
+                    let friendship = this._mapFriendship(objRet.resp._id, objRet.resp._source);
+                    return { code: 200, message: null, resp: friendship };
+                }
+            }, (error) => { return Promise.reject(error); });
     }
 
     getByPlayerId(playerId) {
-        return new Promise((resolve, reject) => {
-            this.esclient.search({
-                "index": "yojuego",
-                "type": "friendship",
-                "body": {
-                    "query": {
-                        "bool": {
-                            "filter": [
-                                { "term": { "playerId": playerId } }
-                            ]
-                        }
-                    }
-                }
-            }, (error, response) => {
-                if (error) {
-                    reject({ code: error.statusCode, message: error.message, resp: error });
-                }
-                else {
-                    let friendships = [];
+        return super.getBy(this._getQueryByPlayerId(playerId), 'yojuego', 'friendship')
+            .then((objRet) => {
+                let friendships = [];
 
-                    for (let i = 0; i < response.hits.hits.length; i++) {
-                        let source = response.hits.hits[i]._source;
-                        let friendship = new Friendship(source.playerId, source.friendId, source.status, source.email);
-                        friendship._id = response.hits.hits[i]._id;
-                        friendship.friendshipAudit = source.friendshipAudit;
-
-                        friendships.push(friendship);
-                    }
-
-                    resolve({ code: 200, message: null, resp: friendships });
+                for (let i = 0; i < objRet.resp.length; i++) {
+                    let friendship = this._mapFriendship(objRet.resp[i]._id, objRet.resp[i]._source);
+                    friendships.push(friendship);
                 }
-            });
-        });
+
+                return { code: 200, message: null, resp: friendships };
+            }, (error) => { return Promise.reject(error); });
     }
 
     getByFriendId(friendId) {
-        return new Promise((resolve, reject) => {
-            this.esclient.search({
-                "index": "yojuego",
-                "type": "friendship",
-                "body": {
-                    "query": {
-                        "bool": {
-                            "filter": [
-                                { "term": { "friendId": friendId } }
-                            ]
-                        }
-                    }
-                }
-            }, (error, response) => {
-                if (error) {
-                    reject({ code: error.statusCode, message: error.message, resp: error });
-                }
-                else {
-                    let friendships = [];
+        return super.getBy(this._getQueryByFriendId(friendId), 'yojuego', 'friendship')
+            .then((objRet) => {
+                let friendships = [];
 
-                    for (let i = 0; i < response.hits.hits.length; i++) {
-                        let source = response.hits.hits[i]._source;
-                        let friendship = new Friendship(source.playerId, source.friendId, source.status, source.email);
-                        friendship._id = response.hits.hits[i]._id;
-                        friendship.friendshipAudit = source.friendshipAudit;
-
-                        friendships.push(friendship);
-                    }
-
-                    resolve({ code: 200, message: null, resp: friendships });
+                for (let i = 0; i < objRet.resp.length; i++) {
+                    let friendship = this._mapFriendship(objRet.resp[i]._id, objRet.resp[i]._source);
+                    friendships.push(friendship);
                 }
-            });
-        });
+
+                return { code: 200, message: null, resp: friendships };
+            }, (error) => { return Promise.reject(error); });
     }
 
     add(friendship) {
         if (friendship instanceof Friendship) {
             return super.add(friendship, 'yojuego', 'friendship');
         } else {
-            return Promise.reject({ code: 410, message: FriendshipESRepository.INVALID_INSTANCE_FRIENDSHIP });
+            return Promise.reject({ code: 410, message: FriendshipESRepository.INVALID_INSTANCE_FRIENDSHIP, resp: null });
         }
     }
 
     update(friendship) {
         if (friendship instanceof Friendship) {
-            let document = {
-                playerId: friendship.playerId,
-                friendId: friendship.friendId,
-                status: friendship.status,
-                email: friendship.email,
-                friendshipAudit: {
-                    createdBy: friendship.friendshipAudit.createdBy,
-                    createdOn: friendship.friendshipAudit.createdOn,
-                    createdFrom: friendship.friendshipAudit.createdFrom,
-                    modifiedBy: friendship.friendshipAudit.modifiedBy,
-                    modifiedOn: friendship.friendshipAudit.modifiedOn,
-                    modifiedFrom: friendship.friendshipAudit.modifiedFrom
-                }
-            };
+            let document = this._getDocument(friendship);
             return super.update(friendship._id, document, 'yojuego', 'friendship');
         } else {
-            return Promise.reject({ code: 410, message: FriendshipESRepository.INVALID_INSTANCE_FRIENDSHIP });
+            return Promise.reject({ code: 410, message: FriendshipESRepository.INVALID_INSTANCE_FRIENDSHIP, resp: null });
         }
     }
 
@@ -130,6 +74,53 @@ class FriendshipESRepository extends ESRepository {
         } else {
             return Promise.reject({ code: 410, message: FriendshipESRepository.INVALID_INSTANCE_FRIENDSHIP });
         }
+    }
+
+    _getDocument(friendship) {
+        let document = {
+            playerId: friendship.playerId,
+            friendId: friendship.friendId,
+            status: friendship.status,
+            email: friendship.email,
+            friendshipAudit: {
+                createdBy: friendship.friendshipAudit.createdBy,
+                createdOn: friendship.friendshipAudit.createdOn,
+                createdFrom: friendship.friendshipAudit.createdFrom,
+                modifiedBy: friendship.friendshipAudit.modifiedBy,
+                modifiedOn: friendship.friendshipAudit.modifiedOn,
+                modifiedFrom: friendship.friendshipAudit.modifiedFrom
+            }
+        };
+
+        return document;
+    }
+
+    _getQueryByFriendId(friendId) {
+        return {
+            "bool": {
+                "filter": [
+                    { "term": { "friendId": friendId } }
+                ]
+            }
+        };
+    }
+
+    _getQueryByPlayerId(playerId) {
+        return {
+            "bool": {
+                "filter": [
+                    { "term": { "playerId": playerId } }
+                ]
+            }
+        };
+    }
+
+    _mapFriendship(id, source) {
+        let friendship = new Friendship(source.playerId, source.friendId, source.status, source.email);
+        friendship._id = id;
+        friendship.friendshipAudit = source.friendshipAudit;
+
+        return friendship;
     }
 
     static get INVALID_INSTANCE_FRIENDSHIP() {
