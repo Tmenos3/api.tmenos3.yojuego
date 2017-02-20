868cc9e345bac9403a7404bcf14a80fb199c3ba4
Facundo La Rocca <frocca@web.web.com>
2017-02-20 17:56:14 -0300

    Check for friendship when create a group

--- src/routes/GroupRoutes.js
+++ src/routes/GroupRoutes.js
@@ -2,12 +2,11 @@ let Validator = require('no-if-validator').Validator;
 let NotNullOrUndefinedCondition = require('no-if-validator').NotNullOrUndefinedCondition;
 let Routes = require('./Routes');
 let Group = require('../models/Group');
-let Player = require('../models/Player');
 let GroupRepository = require('../repositories/GroupESRepository');
-let PlayerRepository = require('../repositories/PlayerESRepository');
+let FriendshipRepository = require('../repositories/FriendshipESRepository');
 let moment = require('moment');
 
-let repoPlayer = null;
+let repoFriendship = null;
 let repoGroup = null;
 
 class GroupRoutes extends Routes {
@@ -23,20 +22,21 @@ class GroupRoutes extends Routes {
         this._deleteGroup = this._deleteGroup.bind(this);
         this._makeAdminPlayer = this._makeAdminPlayer.bind(this);
         this._checkPlayerMember = this._checkPlayerMember.bind(this);
+        this._checkPlayerFriends = this._checkPlayerFriends.bind(this);
 
         let validator = new Validator();
         validator.addCondition(new NotNullOrUndefinedCondition(esClient).throw(GroupRoutes.INVALID_ES_CLIENT));
 
         validator.execute(() => {
             repoGroup = new GroupRepository(esClient);
-            repoPlayer = new PlayerRepository(esClient);
+            repoFriendship = new FriendshipRepository(esClient);
         }, (err) => { throw err; });
     }
 
     _addAllRoutes(server) {
         server.get('/group/:id', super._paramsIsNotNull, this._getGroup, this._checkPlayerMember, (req, res, next) => { res.json(200, { code: 200, resp: req.group, message: 'Group created' }) });
         server.get('/group', this._getAllGroups, (req, res, next) => { res.json(200, { code: 200, resp: req.groups, message: null }) });
-        server.post('/group/create', super._bodyIsNotNull, this._createGroup, (req, res, next) => { res.json(200, { code: 200, resp: req.group, message: null }) });
+        server.post('/group/create', super._bodyIsNotNull, this._checkPlayerFriends, this._createGroup, (req, res, next) => { res.json(200, { code: 200, resp: req.group, message: null }) });
         server.post('/group/:id/addPlayer', super._paramsIsNotNull, this._getGroup, this._addPlayer, this._updateGroup, (req, res, next) => { res.json(200, { code: 200, resp: req.group, message: null }) });
         server.post('/group/:id/removePlayer', super._paramsIsNotNull, this._getGroup, this._removePlayer, this._updateGroup, (req, res, next) => { res.json(200, { code: 200, resp: req.group, message: null }) });
         server.post('/group/:id/makeAdminPlayer', super._paramsIsNotNull, this._getGroup, this._makeAdminPlayer, this._updateGroup, (req, res, next) => { res.json(200, { code: 200, resp: req.group, message: null }) });
@@ -181,6 +181,28 @@ class GroupRoutes extends Routes {
             res.json(400, { code: 400, message: 'El player no es miembro del group.', resp: null });
     }
 
+    _checkPlayerFriends(req, res, next) {
+        repoFriendship.getByPlayerId(req.player._id)
+            .then((resp) => {
+                let notFriends = [];
+
+                for (let i = 0; i < req.body.players.length; i++) {
+                    if (!this._isFriend(req.body.players[i], resp.resp))
+                        notFriends.push(req.body.players[i]._id);
+                }
+
+                if (notFriends.length > 0)
+                    res.json(404, { code: 404, message: 'There are player who are not your friends, they must accept your friendship request before you can invite them.', resp: notFriends });
+                else
+                    next();
+            }, (cause) => {
+                res.json(404, { code: 404, message: cause, resp: null });
+            })
+            .catch((err) => {
+                res.json(500, { code: 500, message: err, resp: null });
+            });
+    }
+
     _updateGroup(req, res, next) {
         req.group.groupAudit.modifiedBy = req.player._id; //We should store deviceId here
         req.group.groupAudit.modifiedOn = new Date();
@@ -196,6 +218,14 @@ class GroupRoutes extends Routes {
             .catch((err) => {
                 res.json(500, { code: 500, message: err, resp: null });
             });
+    }
+
+    _isFriend(id, friends) {
+        for (let i = 0; i < friends.length; i++) {
+            if (friends[i].friendId == id)
+                return true;
+        }
+        return false;
     }
 
     static get INVALID_ES_CLIENT() {
