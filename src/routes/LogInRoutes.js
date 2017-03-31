52e3b96fbec537eca64d98a6b183303f18bda7bb
Facundo La Rocca <facundo_larocca@yahoo.com.ar>
2017-03-30 23:27:26 -0300

    Renew Token Route

--- src/routes/LogInRoutes.js
+++ src/routes/LogInRoutes.js
@@ -19,6 +19,10 @@ class LogInRoutes extends Routes {
         this._generateToken = this._generateToken.bind(this);
         this._auditUser = this._auditUser.bind(this);
         this._getPlayer = this._getPlayer.bind(this);
+        this._getUser = this._getUser.bind(this);
+        this._verifyToken = this._verifyToken.bind(this);
+        this._sendResponse = this._sendResponse.bind(this);
+        this._verifyTokenOwnedByUser = this._verifyTokenOwnedByUser.bind(this);
 
         let validator = new Validator();
         validator.addCondition(new NotNullOrUndefinedCondition(esClient).throw(LogInRoutes.INVALID_ES_CLIENT));
@@ -45,14 +49,31 @@ class LogInRoutes extends Routes {
             this._getPlayer,
             this._generateToken,
             this._auditUser,
-            (req, res, next) => {
-                let resp = {
-                    token: req.token,
-                    user: req.user,
-                    player: req.player
-                }
-                res.json(200, resp);
-            });
+            this._sendResponse);
+
+        server.post('/login/renewtoken',
+            super._bodyIsNotNull,
+            this._verifyToken,
+            this._getUser,
+            this._verifyTokenOwnedByUser,
+            this._getPlayer,
+            this._generateToken,
+            this._auditUser,
+            this._sendResponse);
+    }
+
+    _sendResponse(req, res, next) {
+        req.user.userAudit = undefined;
+        req.user.password = undefined;
+        req.user.token = undefined;
+        req.player.playerAudit = undefined;
+
+        let resp = {
+            token: req.token,
+            user: req.user,
+            player: req.player
+        }
+        res.json(200, resp);
     }
 
     _getPlayer(req, res, next) {
@@ -66,6 +87,26 @@ class LogInRoutes extends Routes {
             .catch((err) => {
                 res.json(500, { code: 500, message: err, resp: null });
             });
+    }
+
+    _getUser(req, res, next) {
+        userRepo.get(req.user._id, 'yojuego')
+            .then((response) => {
+                req.user = response.resp;
+                next();
+            }, (err) => {
+                res.json(400, { code: 400, message: err, resp: null });
+            })
+            .catch((err) => {
+                res.json(500, { code: 500, message: err, resp: null });
+            });
+    }
+
+    _verifyTokenOwnedByUser(req, res, next) {
+        if (req.user.token !== req.body.token)
+            res.json(400, { code: 400, message: 'Unauthorized access.', resp: null });
+        else
+            next();
     }
 
     _validateLogin(req, res, next) {
@@ -98,6 +139,18 @@ class LogInRoutes extends Routes {
         req.user.token = req.token;
         req.user.isLogged = true;
         next();
+    }
+
+    _verifyToken(req, res, next) {
+        try {
+            req.data = jwt.verify(req.body.token, config.get('serverConfig').secret);
+            req.user = {
+                _id: req.data.id
+            };
+            next();
+        } catch (error) {
+            res.json(400, { code: 400, message: 'Unauthorized access.', resp: null });
+        }
     }
 
     _auditUser(req, res, next) {
