e1a3d30e9ecd2410035cea8b6b1abb4411a3e95b
Facundo La Rocca <facundo_larocca@yahoo.com.ar>
2017-04-24 22:51:33 -0300

    Update device id

--- src/routes/DeviceRegistrationRoutes.js
+++ src/routes/DeviceRegistrationRoutes.js
@@ -17,6 +17,7 @@ class DeviceRegistrationRoutes extends Routes {
         this._verifyDeviceExists = this._verifyDeviceExists.bind(this);
         this._checkUser = this._checkUser.bind(this);
         this._deleteDevice = this._deleteDevice.bind(this);
+        this._updateDevice = this._updateDevice.bind(this);
 
         let validator = new Validator();
         validator.addCondition(new NotNullOrUndefinedCondition(esClient).throw(DeviceRegistrationRoutes.INVALID_ES_CLIENT));
@@ -29,6 +30,7 @@ class DeviceRegistrationRoutes extends Routes {
 
     _addAllRoutes(server) {
         server.post('/device/register', super._bodyIsNotNull, this._checkDeviceAndPlatform, this._checkIfDeviceExists, this._registerDevice);
+        server.post('/device/:id', super._paramsIsNotNull, super._bodyIsNotNull, this._verifyDeviceExists, this._checkUser, this._updateDevice);
         server.del('/device/:id', super._paramsIsNotNull, this._verifyDeviceExists, this._checkUser, this._deleteDevice);
         server.post('/device/sendTest', (req, res, next) => {
             ns.notify(null);
@@ -107,7 +109,28 @@ class DeviceRegistrationRoutes extends Routes {
     _deleteDevice(req, res, next) {
         repo.delete(req.device)
             .then((response) => {
-                next();
+                res.json(200, { code: 200, message: 'Device deleted.', resp: null });
+            }, (cause) => {
+                res.json(400, { code: cause.code, message: cause.message, resp: null });
+            })
+            .catch((error) => {
+                res.json(500, { code: error.code, message: error.message, resp: null });
+            });
+    }
+
+    _updateDevice(req, res, next) {
+        req.device.deviceId = req.body.deviceId;
+        req.device.deviceAudit = {
+            createdBy: req.device.deviceAudit.createdBy,
+            createdOn: req.device.deviceAudit.createdOn,
+            createdFrom: req.device.createdFrom.createdBy /* This should be added to the token */,
+            modifiedBy: req.user.id,
+            modifiedOn: new Date(),
+            modifiedFrom: 'MOBILE' /* This should be added to the token */
+        }
+        repo.update(req.device)
+            .then((response) => {
+                res.json(200, { code: 200, message: 'Device updated.', resp: null });
             }, (cause) => {
                 res.json(400, { code: cause.code, message: cause.message, resp: null });
             })
